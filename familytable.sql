2ac14d1d8eb5e01b6575264e491b4e095fd93a8d
webwithsumit <150454643+webwithsumit@users.noreply.github.com>
2024-06-06 21:50:07 +0530

    .

--- familytable.sql
+++ familytable.sql
@@ -1,6 +1,10 @@
 use college;
-create table family(
-id int primary key, name varchar(30),relation varchar(30),city varchar(20),rating int
+CREATE TABLE family (
+    id INT PRIMARY KEY,
+    name VARCHAR(30),
+    relation VARCHAR(30),
+    city VARCHAR(20),
+    rating INT
 );
 insert into family 
 values(001,'Sumit','myself','bhopal',10),
