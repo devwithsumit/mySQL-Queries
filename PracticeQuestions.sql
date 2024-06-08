ebee46de755045748be8f05c4ff8d8911a49a071
webwithsumit <150454643+webwithsumit@users.noreply.github.com>
2024-06-08 22:34:22 +0530

    .

--- PracticeQuestions.sql
+++ PracticeQuestions.sql
@@ -1,8 +1,14 @@
-select name from country where gmax(population);
-use world;
-select name,population from country where population >= 500000 or population <= 1000000;
-Select max(population) from country;
-Select name from country order by surfacearea desc limit 5,10;
-Select max(population) from country order by population;
-select name from country;
-Select population from country order by population limit 1,1;
+-- 1 display name of students with age between 18 to 21;
+ SELECT name,age from college.students where age >= 18 and age <=21;
+ 
+-- 2 display name of maximum rollno/marks or highest rollno/marks
+select name,rollno from students order by rollno desc limit 1;
+
+-- 3 display name of coutries whose name starts with 'a' and ends with 'a'
+select name from world.country where name like 'a%a';
+
+-- 4 select customers from bhopal indore or gwalior
+select name from college.students where city = 'Bhopal' or city = 'Indore' or city  = 'Gwalior';
+
+-- 5 find the vehicle whose color is blue and name starts with 'mp04' and ends with '28'
+select * from vehicle where name like 'mp04%28' and colour = 'blue';
